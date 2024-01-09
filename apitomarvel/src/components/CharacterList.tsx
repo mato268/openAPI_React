4d826ccd30bca6e67f03ba01fc55415165bd6fbe
Sangjun Lee <mato268@naver.com>
2024-01-09 16:38:04 +0900

    CharacterList.tsx

---
+++ apitomarvel/src/components/CharacterList.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+
+  function request(path: string) {
+    return fetch(`https://gateway.marvel.com:443/v1/public/characters?${path}`);
+  }
+
+  useEffect(() => {
+    async function getCharacters() {
+      try {
+        const response1 = await request(
+          "apikey=aefc5f2cd24e5ccc94b27767e260b6d9"
+        );
+        const data1 = await response1.json();
+        console.log(data1);
+        return data1;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getCharacters().then(response => setCharacters(characters));
+  }, []);
+
+  return (
+    <ul>
+      <li></li>
+    </ul>
+  );
+}
