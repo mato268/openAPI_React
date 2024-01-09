d2032de7a0a9f5e36b03abe46b5713b6d2bde983
Sangjun Lee <mato268@naver.com>
2024-01-09 16:36:57 +0900

    App.tsx

---
+++ apitomarvel/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CharacterList from "./components/CharacterList";
+
+function App() {
+  return (
+    <main>
+      <CharacterList />
+    </main>
+  );
+}
+
+export default App;
