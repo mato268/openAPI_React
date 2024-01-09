e6ddf41cb2e723afd92691de779109215168a3e6
Sangjun Lee <mato268@naver.com>
2024-01-09 12:53:46 +0900

    ADD [MatchList.tsx]

--- apitoreact/src/components/MatchList.tsx
+++ apitoreact/src/components/MatchList.tsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function MatchList() {
+  const [list, setList] = useState("");
+
   const API_KEY: string =
     "test_efb42ba6f037a09fa65e28125f10993901f1b04a6a72fa38874476df6649b33021322271e4aa69d519c1f1c287a7bf59";
 
@@ -10,27 +15,34 @@ export default function MatchList() {
     });
   }
 
-  async function getMatchList() {
-    try {
-      // 모든 매치 기록 조회
-      const response1 = await request(
-        "match?matchtype=30&offset=0&limit=100&orderby=desc"
-      );
-      const data1 = await response1.json();
-      console.log(data1);
-      for (let list of data1) {
-        // 매치 상세 기록 조회
-        const response2 = await request("match-detail?matchid=" + list);
+  useEffect(() => {
+    const getMatchList = async () => {
+      try {
+        const response1 = await request(
+          "match?matchtype=30&offset=0&limit=100&orderby=desc"
+        );
+        const data1 = await response1.json();
+        console.log(data1);
+        const response2 = await request("match-detail?matchid=" + data1[0]);
         const data2 = await response2.json();
         console.log(data2);
         return data2;
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-    }
+    };
+
+    getMatchList().then(response => setList(response));
+  }, []);
+
+  interface Item {
+    matctDate: string;
+    matcgId:string;
   }
 
-  getMatchList();
-  
-  return <h1>d</h1>;
+  return (
+    <ul>
+      <li></li>
+    </ul>
+  );
 }
