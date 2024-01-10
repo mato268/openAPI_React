e1831589eaaef6864f4957fc8173af8df5f94ef9
Sangjun Lee <mato268@naver.com>
2024-01-10 15:55:14 +0900

    CharacterList.tsx

--- apitomarvel/src/components/CharacterList.tsx
+++ apitomarvel/src/components/CharacterList.tsx
@@ -4,6 +4,10 @@ import UseFetch from "./UseFetch";
 export default function CharacterList() {
   const characters = UseFetch();
 
+  if (characters.length === 0) {
+    return <span>캐릭터 리스트를 불러오는 중입니다...</span>;
+  }
+
   return (
     <ul>
       {characters.map((item, id) => (
