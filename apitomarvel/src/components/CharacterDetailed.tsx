04ed4a2dc1e2ff31abb02229cf12382ab7c9907e
Sangjun Lee <mato268@naver.com>
2024-01-10 15:48:28 +0900

    CharacterDetailed.tsx

--- apitomarvel/src/components/CharacterDetailed.tsx
+++ apitomarvel/src/components/CharacterDetailed.tsx
@@ -1,43 +1,26 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import UseFetch from "./UseFetch";
 
 export default function CharacterDetailed() {
-  const [characters, setCharacters] = useState<any[]>([]);
-
-  function request(path: string) {
-    return fetch(`https://gateway.marvel.com:443/v1/public/characters?${path}`);
-  }
-
-  useEffect(() => {
-    async function getCharacters() {
-      try {
-        const response1 = await request(
-          "apikey=aefc5f2cd24e5ccc94b27767e260b6d9"
-        );
-        const data1 = await response1.json();
-        console.log(data1);
-        return data1.data.results;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    getCharacters().then(response => setCharacters(response));
-  }, []);
+  const characters = UseFetch();
 
   const list = useParams().id;
   console.log(list);
-  const CharacterList = characters.filter(itemid => itemid.id === Number(list));
+  const characterList = characters.filter(itemid => itemid.id === Number(list));
+
+  if (characters.length === 0) {
+    return <span>캐릭터 정보를 불러오는 중입니다...</span>;
+  }
 
   return (
     <>
       <table>
         <tbody>
-          {CharacterList.map((item, id) => (
-              <tr key={id}>
-                <td>번호 : {item.id}</td>
-                <td>이름 : {item.name}</td>
-              </tr>
+          {characterList.map((item, id) => (
+            <tr key={id}>
+              <td>번호 : {item.id}</td>
+              <td>이름 : {item.name}</td>
+            </tr>
           ))}
         </tbody>
       </table>
