9c209435a472c4f5aa8152dbe0ec8cb7a432b52c
Sangjun Lee <mato268@naver.com>
2024-01-10 21:07:54 +0900

    App.tsx

--- apitomarvel/src/App.tsx
+++ apitomarvel/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CharacterList from "./components/CharacterList";
 import CharacterDetailed from "./components/CharacterDetailed";
 import Header from "./components/Header";
+import WrongApproachPage from "./components/WrongApproachPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
         <Route path="/" element={<CharacterList />}></Route>
         <Route path="/characterdetailed/:id" element={<CharacterDetailed />}></Route>
+        <Route element={<WrongApproachPage />}></Route>
         </Routes>
       </main>
     </BrowserRouter>
