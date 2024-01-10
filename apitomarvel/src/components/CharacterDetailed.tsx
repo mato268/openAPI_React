6c8bc19d83f3099eb5d96e26d8e56c296df7a707
Sangjun Lee <mato268@naver.com>
2024-01-10 14:03:18 +0900

    CharacterDetailed.tsx

---
+++ apitomarvel/src/components/CharacterDetailed.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function CharacterDetailed() {
+  const [characters, setCharacters] = useState<any[]>([]);
+
+  function request(path: string) {
+    return fetch(`https://gateway.marvel.com:443/v1/public/characters?${path}`);
+  }
+
+  useEffect(() => {
+    async function getCharacters() {
+      try {
+        const response1 = await request(
+          "apikey=aefc5f2cd24e5ccc94b27767e260b6d9"
+        );
+        const data1 = await response1.json();
+        console.log(data1);
+        return data1.data.results;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getCharacters().then(response => setCharacters(response));
+  }, []);
+
+  const list = useParams().id;
+  console.log(list);
+  const CharacterList = characters.filter(itemid => itemid.id === Number(list));
+
+  return (
+    <>
+      <table>
+        <tbody>
+          {CharacterList.map((item, id) => (
+            <>
+              <tr key={id}>
+                <td>번호 : {item.id} </td>
+                <td>이름 : {item.name} </td>
+              </tr>
+            </>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
