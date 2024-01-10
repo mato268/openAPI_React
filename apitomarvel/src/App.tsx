7efd0fd01d0344658c42d5470340dce50808db81
Sangjun Lee <mato268@naver.com>
2024-01-10 14:01:17 +0900

    App.tsx

--- apitomarvel/src/App.tsx
+++ apitomarvel/src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CharacterList from "./components/CharacterList";
+import CharacterDetailed from "./components/CharacterDetailed";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <main>
-      <CharacterList />
-    </main>
+    <BrowserRouter>
+      <main>
+        <Header />
+        <Routes>
+        <Route path="/" element={<CharacterList />}></Route>
+        <Route path="/characterdetailed/:id" element={<CharacterDetailed />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
