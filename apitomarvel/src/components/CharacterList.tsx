9cefa2583eedc7d9e8ffeef5d37c94357e2748de
Sangjun Lee <mato268@naver.com>
2024-01-10 15:48:12 +0900

    CharacterList.tsx

--- apitomarvel/src/components/CharacterList.tsx
+++ apitomarvel/src/components/CharacterList.tsx
@@ -1,29 +1,8 @@
-import { useEffect, useState } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import UseFetch from "./UseFetch";
 
 export default function CharacterList() {
-  const [characters, setCharacters] = useState<any[]>([]);
-
-  function request(path: string) {
-    return fetch(`https://gateway.marvel.com:443/v1/public/characters?${path}`);
-  }
-
-  useEffect(() => {
-    async function getCharacters() {
-      try {
-        const response1 = await request(
-          "apikey=aefc5f2cd24e5ccc94b27767e260b6d9"
-        );
-        const data1 = await response1.json();
-        console.log(data1);
-        return data1.data.results;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    getCharacters().then(response => setCharacters(response));
-  }, []);
+  const characters = UseFetch();
 
   return (
     <ul>
