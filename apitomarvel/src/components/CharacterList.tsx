47781d7a9a7d6cd64d6257432f816ea127b9f01f
Sangjun Lee <mato268@naver.com>
2024-01-12 21:23:58 +0900

    commit

--- apitomarvel/src/components/CharacterList.tsx
+++ apitomarvel/src/components/CharacterList.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./css/CharacterList.css";
+import "./css/CharacterList.module.css";
 import { Pagenation } from "./Pagenation";
 import { useMarvelList } from "../query/useMarvelList";
 
@@ -11,18 +11,23 @@ export default function CharacterList() {
     return <span>캐릭터 리스트를 불러오는 중입니다...</span>;
   }
 
+  console.log(data);
+
   return (
     <>
       <ul>
-        {data?.results.map((item) => (
-          <li key={item.id}>
-            <Link
-              to={`/characterdetailed/${item.id}`}
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              캐릭터명 : {item.name}
-            </Link>
-          </li>
+        {data?.results.map(item => (
+          <div key={item.id}>
+            <img src={item.thumbnail} alt="profile"/>
+            <li>
+              <Link
+                to={`/characterdetailed/${item.id}`}
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                캐릭터명 : {item.name}
+              </Link>
+            </li>
+          </div>
         ))}
       </ul>
       <Pagenation maxPage={maxPage} page={page} onPageChange={setPage} />
