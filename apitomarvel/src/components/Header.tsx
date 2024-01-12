1b626ccbd3fe4e5e5a0b0426b484aa29b2bb5f7a
Sangjun Lee <mato268@naver.com>
2024-01-12 22:31:46 +0900

    버튼 위치 수정

--- apitomarvel/src/components/Header.tsx
+++ apitomarvel/src/components/Header.tsx
@@ -4,7 +4,7 @@ import "./css/Header.module.css";
 export default function Header() {
   return (
     <h1>
-      <Link to={"/"} style={{ textDecoration: "none" }}>
+      <Link to={"/"} style={{ textDecoration: "none", color: "red" }}>
         마블 캐릭터
       </Link>
     </h1>
